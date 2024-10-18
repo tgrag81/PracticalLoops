bf3e45949c6ee8e345c80c6b5c75d999e85abbb3
Terence A Gragston <terence.gragston@gmail.com>
2024-10-18 13:05:38 -0400

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,12 @@
+function fizzBuzz(num) {
+    let i  = 1;
+    while (i < num) {
+        for (let i%0, i++ ) {q
+            console.log(Fizz)
+        }
+        else if (i % 5 === 0) {
+            console.log(Buzz)
+        }
+    }
+    return 
+}
